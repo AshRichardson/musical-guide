d86216ba4ad03e9de741f94a23f265b7ed1ac320
Ashleigh Richardson <ashleigh.richardson@uqconnect.edu.au>
2018-05-12 17:53:37 +1000

    added key bindings for condition 2

--- musicalguide/study/static/study/js/cond_two.js
+++ musicalguide/study/static/study/js/cond_two.js
@@ -26,6 +26,8 @@ var originalStartTime = d.getTime();
 var thisNoteStart = 0;
 var endTime = 0;
 
+var octaveNum = 4;
+
 function generate_note(path, params) {
 	var form = $('<form></form>');
 	form.attr("method", "post");
@@ -119,6 +121,20 @@ function note_off(noteDiv) {
 }
 
 var main = function() {
+	const lowerOctave = {83:document.querySelectorAll('div.C3')[0], 69:document.querySelectorAll('div.Cs3')[0], 68:document.querySelectorAll('div.D3')[0],
+		82:document.querySelectorAll('div.Ds3')[0], 70:document.querySelectorAll('div.E3')[0], 71: document.querySelectorAll('div.F3')[0],
+		89:document.querySelectorAll('div.Fs3')[0], 72:document.querySelectorAll('div.G3')[0], 85:document.querySelectorAll('div.Gs3')[0],
+		74:document.querySelectorAll('div.A4')[0], 73:document.querySelectorAll('div.As4')[0], 75:document.querySelectorAll('div.B4')[0],
+		76:document.querySelectorAll('div.C4')[0]}
+	const middleOctave = {83:document.querySelectorAll('div.C4')[0], 69:document.querySelectorAll('div.Cs4')[0], 68:document.querySelectorAll('div.D4')[0],
+		82:document.querySelectorAll('div.Ds4')[0], 70:document.querySelectorAll('div.E4')[0], 71: document.querySelectorAll('div.F4')[0],
+		89:document.querySelectorAll('div.Fs4')[0], 72:document.querySelectorAll('div.G4')[0], 85:document.querySelectorAll('div.Gs4')[0],
+		74:document.querySelectorAll('div.A5')[0], 73:document.querySelectorAll('div.As5')[0], 75:document.querySelectorAll('div.B5')[0],
+		76:document.querySelectorAll('div.C5')[0]}
+	const upperOctave = {83:document.querySelectorAll('div.C5')[0], 69:document.querySelectorAll('div.Cs5')[0], 68:document.querySelectorAll('div.D5')[0],
+		82:document.querySelectorAll('div.Ds5')[0], 70:document.querySelectorAll('div.E5')[0], 71: document.querySelectorAll('div.F5')[0],
+		89:document.querySelectorAll('div.Fs5')[0], 72:document.querySelectorAll('div.G5')[0], 85:document.querySelectorAll('div.Gs5')[0],
+		74:document.querySelectorAll('div.A6')[0], 73:document.querySelectorAll('div.As6')[0], 75:document.querySelectorAll('div.B6')[0]}
 
 	document.body.onmousedown = function() {
 		mouseDown = 1;
@@ -145,6 +161,31 @@ var main = function() {
 	$('.piano-key').mouseleave(function() {
 		if (this.style.backgroundColor == 'cyan') {
 			note_off(this);
+		}
+	})
+
+	$(document).keydown(function(e) {
+		if (e.which == 90 && octaveNum > 3) {
+			octaveNum -= 1;
+		} else if (e.which == 88 && octaveNum < 5) {
+			octaveNum += 1;
+		}
+		if (octaveNum == 3 && (e.which in lowerOctave)) {
+			note_on(lowerOctave[e.which]);
+		} else if (octaveNum == 4 && (e.which in middleOctave)) {
+			note_on(middleOctave[e.which]);
+		} else if (octaveNum == 5 && (e.which in upperOctave)) {
+			note_on(upperOctave[e.which]);
+		}
+	})
+
+	$(document).keyup(function(e) {
+		if (octaveNum == 3 && (e.which in lowerOctave)) {
+			note_off(lowerOctave[e.which]);
+		} else if (octaveNum == 4 && (e.which in middleOctave)) {
+			note_off(middleOctave[e.which]);
+		} else if (octaveNum == 5 && (e.which in upperOctave)) {
+			note_off(upperOctave[e.which]);
 		}
 	})
 
