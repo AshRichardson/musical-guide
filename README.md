58a4a728634631b48f1a2babad265d5ece049cc9
Ashleigh Richardson <ashleigh.richardson@uqconnect.edu.au>
2018-11-05 11:16:27 +1000

    Final updates

--- README.md
+++ README.md
@@ -10,6 +10,21 @@ This app was built using the following:
 
 It may work with other versions of these, but no guarantees.
 
+## Demos (no sound)
+URL to mp4 demo of web app (no sound included):
+https://drive.google.com/file/d/1NziPgLeWwrcIuzI5EGW-GU8V4Qeaz5MB/view?usp=sharing
+
+URL to gif demo of web app (no sound included):
+https://drive.google.com/file/d/1ABW3FUXf38XJKmjoAJAO5BFrA5c-t4kd/view?usp=sharing
+
+## Running the app
+- Install dependencies
+- Clone the repository
+- Navigate to musical-guide/musicalguide/study/static/study and create a folder called audio containing all sound files in the required format, or copy and paste a folder of audio samples in to this location. If you skip this step, not sound or responses will occur in the interactions.
+- In the command line, navigate back up to musical-guide/musicalguide
+- Run python3 manage.py runserver 0.0.0.0:8000
+- Open 127.0.0.1:8000/study in your browser
+
 ## Notes
 - The audio files have been excluded. To run the server, an audio folder needs to be placed under musicalguide/study/static/study containing mp3 audio files named as 'noteName.mp3'. E.g. 'C4.mp3' for a C4 audio sample, or 'Cs4.mp3' for a C#4 audio sample. Will need audio samples for all notes C3 through B6.
 - Only tested with Google Chrome on Mac and Windows
--- musicalguide/study/views.py
+++ musicalguide/study/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import PreQuestionnaireForm, PostQuestionnaireForm, Participant, Note, ComputerNote
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_protect
-#from resources.magenta_predict import *
 import time
 from django.contrib import messages
 
@@ -18,7 +17,6 @@ def post_questionnaire(request):
 	print(request.session.get('identifier'))
 	participant = Participant.objects.all().get(pk=request.session.get('identifier'))
 	questionnaire = PostQuestionnaireForm(instance=participant)
-	# print(questionnaire)
 	context={'form':questionnaire}
 	return render(request, 'study/post.html', context)
 
@@ -110,7 +108,6 @@ def cond_two_response(request):
 	if request.POST.get('note') is None:
 		print('HARD CODING C3')
 		return JsonResponse({'notes':[('C3', request.POST.get('startTime'), request.POST.get('duration'))]})
-	# print('GETTING RESPONSE FOR', request.POST.get('note'), request.POST.get('startTime'), request.POST.get('duration'))
 	response = get_midi_data(request.POST.get('note'), 0, request.POST.get('duration'))
 	participant = Participant.objects.all().get(pk=request.session.get('identifier'))
 	newNote = Note(participant=participant, noteName=request.POST.get('note'), startTime=request.POST.get('startTime'), duration=request.POST.get('duration'), interaction='magenta')
@@ -129,7 +126,13 @@ def cond_two_response(request):
 		notes.append((note[0], note[1], note[2] - note[1]))
 	return JsonResponse({'notes':notes})
 
-#----------- REFACTOR BELOW PART INTO MAGENTA_PREDICT.PY EVENTUALLY; TODO -----------#
+#--------------------------------------------------------------------------#
+# The code below is adapted from the following source:                     #
+# Title: aiDuet                                                            #
+# Author: Google                                                           #
+# Date: 04/05/18                                                           #
+# Availability: https://github.com/googlecreativelab/aiexperiments-ai-duet #
+#--------------------------------------------------------------------------#
 
 import magenta
 
@@ -146,7 +149,6 @@ import pretty_midi
 import numpy as np
 
 BUNDLE_NAME = 'basic_rnn'
-# BUNDLE_NAME = 'attention_rnn'
 
 noteNameToMidi = {'C3':48, 'C#3':49, 'D3':50, 'D#3':51, 'E3':52, 'F3':53,
 				 'F#3':54, 'G3':55, 'G#3':56, 'A4':57, 'A#4':58, 'B4':59,
@@ -172,8 +174,6 @@ generator = melody_rnn_sequence_generator.MelodyRnnSequenceGenerator(
       bundle=bundle_file)
 
 def generate_midi(midi_data, total_seconds=10):
-    # print('Midi data:', [semitone for semitone in midi_data.get_chroma()])
-    # print('Midi data:', midi_data)
     primer_sequence = magenta.music.midi_io.midi_to_sequence_proto(midi_data)
 
     # For now, assume 120 quavers per minute
@@ -182,20 +182,11 @@ def generate_midi(midi_data, total_seconds=10):
     primer_sequence.tempos[0].qpm = qpm
 
     generator_options = generator_pb2.GeneratorOptions()
-    # Set the start time to begin on the next step after the last note ends.
-    # print('PRIMER NOTES', [n for n in primer_sequence.notes])
     last_end_time = (max(n.end_time for n in primer_sequence.notes)
                      if primer_sequence.notes else 0)
 
-
-
     starting_time = last_end_time + 1 * 60.0 / qpm / steps_per_quarter
-    # print('\n\nSTART AND END ARE:', starting_time, starting_time + total_seconds, '\n\n')
     generator_options.generate_sections.add(start_time=starting_time, end_time=starting_time + total_seconds)
-
-
-    # generator_options.generate_sections.add(start_time=last_end_time + 1 * 60.0 / qpm / steps_per_quarter, end_time=total_seconds)
-    # generate the output sequence
     generated_sequence = generator.generate(primer_sequence, generator_options)
 
     output = tempfile.NamedTemporaryFile()
@@ -204,7 +195,6 @@ def generate_midi(midi_data, total_seconds=10):
     return pretty_midi.PrettyMIDI(output)
 
 def get_midi_data(userNoteName, start, end):
-	# Just generate one for now
 	print('Time for', userNoteName, 'got:', time.time())
 	if len(userNoteName) == 3:
 		userNoteName = userNoteName[0] + '#' + userNoteName[2]
@@ -217,7 +207,6 @@ def get_midi_data(userNoteName, start, end):
 		return []
 	instrument.notes.append(note)
 	primer.instruments.append(instrument)
-	# print('Time before generate for:', userNoteName, time.time())
 	output = generate_midi(primer, total_seconds=2) # Takes about 4-6 seconds
 	aiNotes = []
 	try:
@@ -228,18 +217,6 @@ def get_midi_data(userNoteName, start, end):
 		aiNotes.append((notePitch, noteStart, noteEnd))
 		print('AI notes are', aiNotes)
 		return aiNotes
-
-		# Below is code to handle more than one note generated
-		# At the moment, just taking first note, as subsequent notes
-		# too short to be played
-		# for note in output.instruments[0].notes:
-		# 	notePitch, noteStart, noteEnd = pretty_midi.note_number_to_name(note.pitch), note.start, note.end
-		# 	if len(notePitch) == 3:
-		# 		notePitch = notePitch[0] + 's' + notePitch[2]
-		# 	aiNotes.append((notePitch, noteStart, noteEnd))
-		# 	print('Note adding', notePitch, noteStart, noteEnd)
-		# print('AI notes are', aiNotes)
-		# return sorted(aiNotes, key=lambda x:x[1])
 	except IndexError:
 		return []
 
